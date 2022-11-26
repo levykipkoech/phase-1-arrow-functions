a6e10cb825ff3861d89e9398eabbe87f092939eb
levy kipkoech <kipkoechlevy096@gmail.com>
2022-11-26 16:05:37 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+ const divide = function (){
+        const num1=2000
+        const num2 = 100
+        return (num1/num2)
+ }
+ let square = (num)=>{
+       let x = 2;
+       return(x*x)
+ }
+ let add =(num)=>{
+    let a = 3 ;
+    let b = 4
+    return a + b 
+ }
+
+  
+  
